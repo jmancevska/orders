7b2dff9c51686737d58bf71211de39d38cbfe0cf
jmancevska <minkic@hotmail.com>
2017-02-13 00:06:44 -0700

    Implemented AsyncListener

--- src/main/java/com/benevity/orders/web/OrdersServlet.java
+++ src/main/java/com/benevity/orders/web/OrdersServlet.java
@@ -1,11 +1,17 @@
 package com.benevity.orders.web;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.concurrent.ThreadPoolExecutor;
 
 import javax.servlet.AsyncContext;
+import javax.servlet.AsyncEvent;
+import javax.servlet.AsyncListener;
+import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -25,13 +31,41 @@ public class OrdersServlet extends HttpServlet {
 	public void doGet(HttpServletRequest request, 
 	                  HttpServletResponse response) {
 		
+		final String sessionId = request.getSession().getId();
+		final String relativeWebPath = "/";
+		AsyncOrdersProcessor ordersProcessor = null;
+		String absoluteDiskPath = getServletContext().getRealPath(relativeWebPath);
+		String fileFullPath = absoluteDiskPath + File.separator + sessionId + ".xml";
+
 
 		response.setContentType("text/html;charset=UTF-8");
 		final AsyncContext asyncContext = request.startAsync();
 		
 		ThreadPoolExecutor executor = (ThreadPoolExecutor) request.getServletContext().getAttribute("executor");
-        //executor.execute(new AsyncOrdersProcessor(aCtx));
-		
+ 
+	    asyncContext.setTimeout(10L * 1000);
+	    asyncContext.addListener(new AsyncListener() {
+			
+			public void onTimeout(AsyncEvent event) throws IOException {
+				writeToResponse(event, "The process timed out.");
+			}
+			
+			public void onStartAsync(AsyncEvent event) throws IOException {
+				// TODO Auto-generated method stub
+			}
+			
+			public void onError(AsyncEvent event) throws IOException {
+				writeToResponse(event, event.getThrowable().getMessage());
+			}
+			
+			public void onComplete(AsyncEvent event) throws IOException {
+				writeToResponse(event, "The process completed. " +
+						"<a href=\"" + sessionId + ".xml\">download file</a>" 
+						 );
+			}
+		});
+
+
 		String company = asyncContext.getRequest().getParameter("company");
 		String user = asyncContext.getRequest().getParameter("user");
 		String startDate = asyncContext.getRequest().getParameter("startDate");
@@ -52,7 +86,7 @@ public class OrdersServlet extends HttpServlet {
 				date = dt.parse(startDate);
 				dStartDate = new Date(date.getTime());
 			} catch (ParseException e) {
-				e.printStackTrace();
+				System.err.println(e.getMessage());
 				errMessage = "Start date is not valid. ";
 			} 
 		}
@@ -62,22 +96,29 @@ public class OrdersServlet extends HttpServlet {
 				date = dt.parse(endDate);
 				dEndDate = new Date(date.getTime());
 			} catch (ParseException e) {
-				e.printStackTrace();
+				System.err.println(e.getMessage());
 				errMessage = errMessage + " End date is not valid. ";
 			} 
 		}
 
 		if (errMessage.length() == 0) {
 			statusMessage = "Please wait for the result set.";
-			executor.execute(new AsyncOrdersProcessor(company, user, dStartDate, dEndDate, country));
+			ordersProcessor = new AsyncOrdersProcessor(company, user, dStartDate, dEndDate, country, fileFullPath);
+			executor.execute(ordersProcessor);
 		}
 		request.setAttribute("error", errMessage);
 		request.setAttribute("status", statusMessage);
 		
 		
 		asyncContext.dispatch("/status.jsp");
-		//asyncContext.complete();
         
 	}
+	
+	private void writeToResponse(AsyncEvent event, String message) throws IOException {
+        ServletResponse response = event.getAsyncContext().getResponse();
+        PrintWriter out = response.getWriter();
+        out.write(message);
+    }
+
 
 }
